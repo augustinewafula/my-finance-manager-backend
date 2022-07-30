f67dbe39c3946a9ed66ac8566fa082c94314e0a5
augustinewafula <augustinetreezy@outlook.com>
2022-07-30 20:23:26 +0300

    create UserTransactionCategory and UserTransactionSubCategory models and controller

---
+++ app/Models/UserTransactionCategory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\HasUuid;
+use App\Traits\StoresUserId;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserTransactionCategory extends Model
+{
+    use HasUuid, HasFactory, StoresUserId;
+
+    public $incrementing = false;
+    protected $keyType = 'uuid';
+}
