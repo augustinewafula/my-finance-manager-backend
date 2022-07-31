cb4f466b864ab95c69851f076aa608a4d8b7d977
augustinewafula <augustinetreezy@outlook.com>
2022-07-31 16:01:39 +0300

    implement IdentifyMpesaTransactionCategory

--- app/Actions/IdentifyMpesaTransactionCategory.php
+++ app/Actions/IdentifyMpesaTransactionCategory.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions;
 
+use App\Models\IdentifiedTransactionCategory;
 use App\Models\TransactionCategory;
 use Illuminate\Support\Str;
 
@@ -9,29 +10,92 @@ class IdentifyMpesaTransactionCategory
 {
     public function execute(string $transactionType): array
     {
-        $transactionType = Str::of($transactionType)->lower()->toString();
+        $transactionType = Str::of($transactionType)
+            ->lower()
+            ->ucfirst()
+            ->toString();
 
-        if (Str::of($transactionType)->contains(['hotel', 'restaurant', 'lodging'])) {
-            $category_id = TransactionCategory::where('name', 'Food')->value('id');
-            return [
+        if ($category = $this->existsInIdentifiedCategories($transactionType)) {
+            return $category;
+        }
+
+        $transactionType = Str::of($transactionType)
+            ->lower()
+            ->toString();
+
+        $preIdentifiedTransactionCategories = $this->getPreIdentifiedTransactionCategories();
+
+        $category = [];
+        foreach ($preIdentifiedTransactionCategories as $preIdentifiedTransactionCategory) {
+            $identified_category = $this->identify($transactionType, $preIdentifiedTransactionCategory['categoryName'], $preIdentifiedTransactionCategory['keywords']);
+            if($identified_category) {
+                $category = $identified_category;
+                $this->storeIdentifiedTransactionCategory($transactionType, $category);
+                break;
+            }
+
+        }
+
+        if (empty($category)) {
+            $category_id = TransactionCategory::where('name', 'Other')->value('id');
+            $category = [
                 'category_id' => $category_id,
                 'sub_category_id' => null,
             ];
         }
 
-        if (Str::of($transactionType)->contains(['supermarket', 'groceries', 'fashion', 'electronics'])) {
-            $category_id = TransactionCategory::where('name', 'Shopping')->value('id');
+        return $category;
+    }
+
+    public function existsInIdentifiedCategories(string $transactionType): bool|array
+    {
+        $identifiedCategories = IdentifiedTransactionCategory::whereSubject($transactionType)->first();
+
+        if ($identifiedCategories) {
             return [
-                'category_id' => $category_id,
-                'sub_category_id' => null,
+                'category_id' => $identifiedCategories->category_id,
+                'sub_category_id' => $identifiedCategories->sub_category_id,
             ];
         }
 
-        $category_id = TransactionCategory::where('name', 'Other')->value('id');
+        return false;
+    }
+
+    /**
+     * @return array[]
+     */
+    private function getPreIdentifiedTransactionCategories(): array
+    {
         return [
-            'category_id' => $category_id,
-            'sub_category_id' => null,
+            ['categoryName' => 'Food', 'keywords' => ['hotel', 'restaurant', 'lodging']],
+            ['categoryName' => 'Travel', 'keywords' => ['airline', 'travel', 'flight', 'bus']],
+            ['categoryName' => 'Shopping', 'keywords' => ['shopping', 'shop', 'store', 'retail', 'supermarket', 'groceries', 'fashion', 'electronics']],
+            ['categoryName' => 'Entertainment', 'keywords' => ['entertainment', 'theatre', 'cinema']],
+            ['categoryName' => 'Transport', 'keywords' => ['transport', 'bus', 'train']],
+            ['categoryName' => 'Bills', 'keywords' => ['bills', 'bill', 'payment']],
         ];
+    }
+
+    public function identify(string $transactionType, string $categoryName, array $keywords): array
+    {
+        if (Str::of($transactionType)->contains($keywords)) {
+            $category_id = TransactionCategory::where('name', $categoryName)->value('id');
+            return [
+                'category_id' => $category_id,
+                'sub_category_id' => null,
+            ];
+        }
+
+        return [];
+    }
+
+    public function storeIdentifiedTransactionCategory(string $transactionType, array $category): void
+    {
+        IdentifiedTransactionCategory::create([
+            'subject' => $transactionType,
+            'transaction_category_id' => $category['category_id'],
+            'transaction_sub_category_id' => $category['sub_category_id'],
+        ]);
     }
 
 }
