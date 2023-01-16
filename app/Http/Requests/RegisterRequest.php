65665fdefd59cc6494bbcaa2296092c2fb57a0a6
augustinewafula <augustinetreezy@outlook.com>
2023-01-16 15:24:29 +0300

    set validation rules for strong password

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterRequest extends FormRequest
 {
@@ -26,7 +27,7 @@ class RegisterRequest extends FormRequest
         return [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
+            'password' => ['required', Password::min(8)->mixedCase()->numbers()->uncompromised()],
         ];
     }
 }
