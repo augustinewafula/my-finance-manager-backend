93badf7af205d174d82afb0ee88668c249b6b9f1
augustinewafula <augustinetreezy@outlook.com>
2023-02-18 16:27:14 +0300

    create BondInterestPayingDateResource

---
+++ app/Http/Resources/BondInterestPayingDateResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BondInterestPayingDateResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'bond_id' => $this->bond_id,
+            'interest_paying_date' => $this->date->toDateString(),
+        ];
+    }
+}
