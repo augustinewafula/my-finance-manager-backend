dfa9a2750b9b95ecc5e2e4a8e7b296d586770c1f
augustinewafula <augustinetreezy@outlook.com>
2023-02-25 15:50:31 +0300

    create test_authenticated_user_can_update_bonds

--- tests/Feature/BondsTest.php
+++ tests/Feature/BondsTest.php
@@ -2,9 +2,14 @@
 
 namespace Tests\Feature;
 
+use App\Models\Bond;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Testing\TestResponse;
 use Tests\TestCase;
 
 class BondsTest extends TestCase
@@ -39,16 +44,19 @@ class BondsTest extends TestCase
 
     public function test_authenticated_user_can_create_bonds(): void
     {
-        $response = $this->actingAs($this->user, 'sanctum')->post('/api/v1/bonds', [
-            'issue_number' => '123456',
-            'coupon_rate' => 5.5,
-            'amount_invested' => 1000,
-            'interest_payment_dates' => "01-01-2021, 01-02-2021, 01-03-2021"
-        ], [
-            'Accept' => 'application/json'
-        ]);
+        $response = $this->createBond();
 
         $response->assertStatus(201);
+        $response->assertJsonStructure([
+            'bond' => [
+                'id',
+                'issue_number',
+                'coupon_rate',
+                'amount_invested',
+                'created_at',
+                'updated_at'
+            ]
+        ]);
     }
 
     public function test_authenticated_user_cannot_create_bonds_with_invalid_dates(): void
@@ -57,7 +65,7 @@ class BondsTest extends TestCase
             'issue_number' => '123456',
             'coupon_rate' => 5.5,
             'amount_invested' => 1000,
-            'interest_payment_dates' => "01-01-20, 01-02-221, 01-3-2021"
+            'interest_payment_dates' => "01-01-202, 01-02-221, 01-3-2021"
         ], [
             'Accept' => 'application/json'
         ]);
@@ -66,8 +74,46 @@ class BondsTest extends TestCase
 
     }
 
+    public function test_authenticated_user_can_update_bonds(): void
+    {
+        $createBondResponse = $this->createBond();
+        $bondId = $createBondResponse->json('bond.id');
+
+        $data = [
+            'issue_number' => '1738',
+            'coupon_rate' => 5.5,
+            'amount_invested' => 1000,
+            'interest_payment_dates' => "01-01-2021, 01-02-2021, 01-03-2021"
+        ];
+        $response = $this->actingAs($this->user, 'sanctum')->put("/api/v1/bonds/$bondId", $data, [
+            'Accept' => 'application/json'
+        ]);
+
+        $response->assertStatus(200);
+        Log::info($response->json());
+        $response->assertJson([
+            'message' => 'Bond updated successfully',
+            'bond' => Arr::except($data, ['interest_payment_dates'])
+        ]);
+    }
+
     private function createUser(): User
     {
         return User::factory()->create(['password' => bcrypt('password')]);
+    }
+
+    /**
+     * @return TestResponse
+     */
+    private function createBond(): TestResponse
+    {
+        return $this->actingAs($this->user, 'sanctum')->post('/api/v1/bonds', [
+            'issue_number' => '123456',
+            'coupon_rate' => 5.5,
+            'amount_invested' => 1000,
+            'interest_payment_dates' => "01-01-2021, 01-02-2021, 01-03-2021"
+        ], [
+            'Accept' => 'application/json'
+        ]);
     }
 }
