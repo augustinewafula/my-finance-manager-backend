48d1e9fa4e10250acc9926be0d4d4f47308d0dde
augustinewafula <augustinetreezy@outlook.com>
2023-02-27 16:11:07 +0300

    add records limit parameter to getUpcomingInterests

--- app/Http/Controllers/Analytics/BondAnalyticsController.php
+++ app/Http/Controllers/Analytics/BondAnalyticsController.php
@@ -105,15 +105,16 @@ class BondAnalyticsController extends Controller
         return $years;
     }
 
-    public function getUpcomingInterests()
+    public function getUpcomingInterests(Request $request)
     {
+        $limit = $request->get('limit', 10);
         $user = Auth::user();
         return BondInterestPayingDate::select('bonds.issue_number', 'bond_interest_paying_dates.date', DB::raw('ROUND((bonds.amount_invested * bonds.coupon_rate / 100) / 2, 2) as interest_amount'))
             ->join('bonds', 'bond_interest_paying_dates.bond_id', '=', 'bonds.id')
             ->where('bonds.user_id', '=', $user->id)
             ->where('bond_interest_paying_dates.date', '>=', Carbon::today())
             ->orderBy('bond_interest_paying_dates.date', 'asc')
-            ->limit(12)
+            ->limit($limit)
             ->get();
     }
 
